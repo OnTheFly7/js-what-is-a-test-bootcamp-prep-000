804c23e84dcc273f79238163b222f0454050527a
Brian Senick <bsenick07@yahoo.com>
2018-01-28 17:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
 console.log
+module.exports = {name, height, message}
